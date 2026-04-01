c3278ed92616380ad1ee4e93e55f496fe0f42dc8
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-04-01 22:09:55 +0600

    register your new service

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using ProductsAPI.Data;
+using ProductsAPI.Services;
 
 var builder = WebApplication.CreateBuilder();
 
 builder.Services.AddControllers();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
