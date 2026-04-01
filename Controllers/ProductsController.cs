f2ae1e21ed73f7a1c9be6b5c2e974febc7cdaf57
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-04-01 18:35:13 +0600

    Add initial API

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using ProductsAPI.Data;
+using ProductsAPI.Models;
+
+namespace ProductsAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController:ControllerBase
+{
+    
+
+    private readonly AppDbContext _context;
+
+    public ProductsController(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    
+    [HttpGet]
+    public IActionResult GetProducts()
+    {
+        return Ok(_context.Products);
+    }
+
+    [HttpPost]
+    public IActionResult AddProduct(Product product)
+    {
+
+        _context.Products.Add(product);
+        _context.SaveChanges();
+        return Ok(_context.Products);
+    }
+
+
+    [HttpGet("{id}")]
+        public IActionResult GetProductbyId(int id)
+        {
+            
+            var product = _context.Products.FirstOrDefault(p => p.ID == id);
+            return Ok(product);
+        }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateProduct(Product update, int id)
+    {
+        var res = _context.Products.FirstOrDefault(p => p.ID == id);
+
+        res.Name = update.Name; 
+        res.Price = update.Price;
+        _context.SaveChanges();
+
+        return Ok(res);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteProduct(int id)
+    {
+        var delete = _context.Products.FirstOrDefault(p => p.ID == id);
+
+        _context.Products.Remove(delete);
+        _context.SaveChanges();
+
+        return Ok(_context.Products);
+    }
+    
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsAPI.Models;
+
+namespace ProductsAPI.Data;
+
+public class AppDbContext:DbContext 
+{
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Product> Products { get; set;}
+    
+}
