4229ff9f827378640beed8ade8294b4319025f02
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-04-02 09:16:01 +0600

    Register the Repository into the Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ProductsAPI.Data;
+using ProductsAPI.Repositories;
 using ProductsAPI.Services;
 
 var builder = WebApplication.CreateBuilder();
@@ -10,6 +11,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
