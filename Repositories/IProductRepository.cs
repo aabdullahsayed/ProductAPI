dbcfb31e6696e2f231923e0912633b2332817ed7
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-04-02 09:15:23 +0600

    Add a Repository layer

---
+++ Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProductsAPI.Models;
+
+namespace ProductsAPI.Repositories;
+
+public interface IProductRepository
+{
+    IEnumerable<Product> GetAll();
+    Product GetById(int id);
+    void Add(Product product);
+    void Update(Product product);
+    void Delete(Product product);
+    bool SaveChanges();
+}
---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,47 @@
+﻿using ProductsAPI.Data;
+using ProductsAPI.Models;
+
+namespace ProductsAPI.Repositories;
+
+public class ProductRepository:IProductRepository
+{
+    private readonly AppDbContext _context;
+    
+    public ProductRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+   public IEnumerable<Product> GetAll()
+    {
+        return _context.Products.ToList();
+    }
+
+    public Product GetById(int id)
+    {
+        return _context.Products.FirstOrDefault(p => p.ID == id);
+    }
+
+    public void Add(Product product)
+    {
+        _context.Add(product);
+        _context.SaveChanges();
+    }
+
+    public void Update(Product product)
+    {
+        _context.Products.Update(product);
+    }
+
+    public void Delete(Product product)
+    {
+        _context.Remove(product);
+    }
+
+   public bool SaveChanges()
+   {
+       return _context.SaveChanges()>0;
+   }
+    
+    
+}
