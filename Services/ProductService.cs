e1614337f3c9563982250af58dab6dedec06f20d
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-04-23 18:27:01 +0600

    Inject the Repository, removing the DbContext

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,52 +1,53 @@
 ﻿using ProductsAPI.Data;
 using ProductsAPI.Models;
+using ProductsAPI.Repositories;
 using ProductsAPI.Services;
 
 public class ProductService:IProductService
 {
-    private readonly AppDbContext _context;
-    public ProductService(AppDbContext context)
+    private readonly IProductRepository _repository;
+    public ProductService(IProductRepository repository)
     {
-        _context = context;
+        _repository = repository;
     }
 
     public IEnumerable<Product> GetAllProducts()
     {
-        return _context.Products;
+        return _repository.GetAll();
     }
 
     public Product GetProductById(int id)
     {
-        var product = _context.Products.FirstOrDefault(p => p.ID == id);
 
-        return product;
+        return _repository.GetById(id);
     }
 
     public Product AddProduct(Product product)
     {
-        _context.Add(product);
-        _context.SaveChanges();
-
+        _repository.Add(product);
+        _repository.SaveChanges();
+        
         return product;
     }
 
     public Product UpdateProduct(int id, Product update)
     {
-        var res = _context.Products.FirstOrDefault(p => p.ID == id);
+        var res = _repository.GetById(id);
 
         res.Name = update.Name; 
         res.Price = update.Price;
-        _context.SaveChanges();
+        
+        _repository.SaveChanges();
+        
         return res;
     }
 
     public bool DeleteProduct(int id)
     {
-        var delete = _context.Products.FirstOrDefault(p => p.ID == id);
+        var res = _repository.GetById(id);
 
-        _context.Products.Remove(delete);
-        _context.SaveChanges();
-
+        _repository.Delete(res);
+        _repository.SaveChanges();
         return true;
     }
     
